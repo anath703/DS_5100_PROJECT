1bc6cdf8c88d7f0f55dd806f07cff9fc3285072b
anath703 <89198979+anath703@users.noreply.github.com>
2021-11-05 21:01:40 -0400

    Add files via upload

--- cleanup.py
+++ cleanup.py
@@ -1,23 +1,30 @@
 """
-Created on Tue Oct 19 13:24:48 2021
 
-@author: anoopnath
+Date: 10/31/2021
+Name: Anoop Nath
+Description: Module 8 Exercise: Data Cleaning
+UVA Computing ID:nux9aq
+Notes: The raw csv contains a log of every NFL game played since 1970. However, there are several missing row, which I want to remove. The log indicates score for the winning team and losing team but I want to convert this to score for the home team and the away team.
 
-notes: This procedure cleans up the data so that it is ready to be used for data visualizations and analysis
 """
 
+
+
 import pandas as pd
+import numpy as np
+
 url = 'https://github.com/anath703/DS_5100_PROJECT/raw/main/nflFullGameLog.csv' #location of raw data file
 df = pd.read_csv(url)
-#print(df.dtypes)
+df2= pd.read_csv('https://github.com/anath703/DS_5100_PROJECT/raw/main/nflFullGameLog_neutral.csv')
+df['Neutral'] = df2['Neutral']
+df['Neutral'] =  np.where(df['Location'] == 'N', True , False) # adding a column that indicates if a game is played in a neutral stadium
+df['Away_Team'] = df['Winner/Tie'].where(df['Location'] == '@', df['Loser/Tie'])
 
 
-#df=pd.read_csv('nflFullGameLog.csv') #reading in csv
-
-df=  df[df['Week'].notna()] #removing empty rows
+df=  df[df['Week'].notna()] #removing rows that have NAs in the "Week" column
 df['Date']= pd.to_datetime(df['Date'] ).dt.date #converting to date time
 
-#creating new columns to indicate which team is home, which team is away, yards scored and turnovers
+#creating new columns to indicate which team is home, which team is away, and yards,points and turnovers for each.
 df['Home_Team'] = df['Loser/Tie'].where(df['Location'] == '@', df['Winner/Tie'])
 df['Points_Home'] = df['Points_Loser'].where(df['Location'] == '@', df['Points_Winner'])
 df['Yards_Home'] = df['Yards_Loser'].where(df['Location'] == '@', df['Yards_Winner'])
@@ -29,6 +36,7 @@ df['Points_Away'] = df['Points_Winner'].where(df['Location'] == '@', df['Points_
 df['Yards_Away'] = df['Yards_Winner'].where(df['Location'] == '@', df['Yards_Loser'])
 df['Turnover_Away'] = df['Turnover_Winner'].where(df['Location'] == '@', df['Turnover_Loser'])
 
+#creating columns to indicate point, yards and turnover differential (Home minus Away)
 df['Points_Diff']= df['Points_Home']-df['Points_Away'] 
 df['Yards_Diff']= df['Yards_Home']-df['Yards_Away'] 
 df['Turnover_Diff']= df['Turnover_Home']-df['Turnover_Away'] 
@@ -36,6 +44,8 @@ df['Turnover_Diff']= df['Turnover_Home']-df['Turnover_Away']
 #removing unneeded/duplicate columns
 df.drop([ 'Winner/Tie', 'Location','Loser/Tie','Points_Winner', 'Points_Loser', 'Yards_Winner','Turnover_Winner', 'Yards_Loser', 'Turnover_Loser'], inplace=True, axis=1)
 
+
+df2= pd.read_csv('https://github.com/anath703/DS_5100_PROJECT/raw/main/nflFullGameLog_neutral.csv')
 #savingto csv
 df.to_csv('cleanedNflFullGameLog.csv')
 
